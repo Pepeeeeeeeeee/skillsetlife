8c095e31fa8768ce04a9be368cfbce4287dbc066
xKumma <hristov.plm@gmail.com>
2023-05-23 18:32:01 +0200

    processpage css

--- src/components/ProcessPage.vue
+++ src/components/ProcessPage.vue
@@ -3,15 +3,10 @@
   <h1>Ako to funguje?</h1>
   <div class="individuals">
     <h2>Pre jednotlivcov</h2> 
-        <div>
+        <div class="sub-plans">
             <SubscriptionPage />
         </div>
     <div class="ind-process">
-      <!-- <div class="step">
-        <div class="step-img"><img src="../assets/log-in.svg" alt=""></div>
-        <h3>Zaregistruj sa</h3>
-      </div>
-      <img src="../assets/arrow-long-right.svg" alt=""> -->
       <div class="step">
         <div class="step-img"><img src="../assets/credit-card.svg" alt=""></div>
         <h3>Vyber si</h3>
@@ -29,20 +24,21 @@
     <div class="org-process">
       <div class="step">
         <div class="step-img"><img src="../assets/notes.svg" alt=""></div>
-        <h3>Vypln formular</h3>
+        <h3>Napiste nam</h3>
       </div>
       <img src="../assets/arrow-long-right.svg" alt="">
       <div class="step">
         <div class="step-img"><img src="../assets/sand-clock.svg" alt=""></div>
-        <h3>Pockaj na odpoved</h3>
+        <h3>Pockajte na odpoved</h3>
       </div>
       <img src="../assets/arrow-long-right.svg" alt="">
       <div class="step">
         <div class="step-img"><img src="../assets/key.svg" alt=""></div>
-        <h3>Ziskaj pristup</h3>
+        <h3>Ziskajte pristup</h3>
       </div>
     </div>
-    <button class = "button1">Napiste nam</button>
+    <h4><a href="#">Napiste nam</a> a spolocne prideme na prijatelnu cenu</h4>
+    <!-- <button class = "button1">Napiste nam</button> -->
   </div>
 </div>
 </template>
@@ -74,18 +70,23 @@ h1 {
 
 h2 {
   font-size: x-large;
+  margin-left: 32px;
 }
 
 .individuals, .organisations{
   display: flex;
   flex-direction: column;
   position: relative;
-  margin: 32px auto 96px 48px;
-  gap: 5em;
+  gap: 3em;
+}
+
+.sub-plans{
+  margin: auto;
 }
 
 .ind-process, .org-process {
   display: flex;
+  margin: auto;
   gap: 2em;
 }
 img {
@@ -95,6 +96,16 @@ img {
 .step {
   text-align: center;
   width: 182px;
+}
+
+h4 {
+  font-size: x-large;
+  text-align: center;
+  margin-top: 0;
+}
+
+a {
+  color: #2395E7;
 }
 
 .step-img{
--- src/components/SubscriptionPage.vue
+++ src/components/SubscriptionPage.vue
@@ -1,13 +1,15 @@
 <template>
     <div class = "subscriptions-container">
-        <div class = "card">
-            <h4 class = "h4">Mesacny</h4>
+        <div class = "card monthly">
+            <p class="sale">/</p>
+            <h4>Mesacny</h4>
             <h2>12.99$</h2>
             <h5>mesacne</h5>
             <button @click = "pay()" class = "button1">Pokraovat</button>
         </div>
-        <div class = "card">
-            <h4 class = "h4">Rocny</h4>
+        <div class = "card yearly">
+            <p class="sale">Usetrite XX%</p>
+            <h4>Rocny</h4>
             <h2>129$</h2>
             <h5>rocne</h5>
             <button @click = "pay()" class = "button1">Pokraovat</button>
@@ -33,21 +35,46 @@ export default ({
     .subscriptions-container{
 	display: flex;
 	flex-direction: row;
+    gap: 5em;
     }
 
     .card{
-        margin-right: 100px;
-        width: 15%;
-        height: 200px;
-        border-style: solid;
-        border-color: black;
+        width: 196px;
+        height: 220px;
+        border: 2px solid black;
+        text-align: center;
+        border-radius: 16px;
     }
     
     .button1{
         width: 90%;
     }
 
-    .h4{
-        padding-top: 5%;
+    h2 {
+        font-size: xx-large;
+        margin-bottom: 0;
+    }
+    h5 {
+        margin-top: 0;
+        font-weight: 500;
+        font-style: italic;
+
+    }
+
+    .card:hover{
+        border-color: #2395E7;
+        color: #2395E7;
+    }
+
+    .sale {
+        box-sizing: border-box;
+        border-radius: 14px 14px 0 0 ;
+        margin: 0;
+        padding: 6px;
+        color: white;
+    }
+
+    .yearly > .sale {
+        background-color: #2395E7;
     }
 </style>
